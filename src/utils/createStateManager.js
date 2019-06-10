a0fe98a6a79f22f44fc951a12cfd768e6f62b2b0
梁柱 <liangzhu@77ircloud.com>
2019-06-10 14:01:52 +0800

    fix: 调整create-state-manager

--- src/utils/createStateManager.js
+++ src/utils/createStateManager.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React, { createContext, useMemo, useContext } from 'react';
 import immer from 'immer';
 
@@ -12,30 +13,26 @@ export default function createStateManager(initalState = {}) {
     // 仅有 state 变更了, 才会重新更新 context 和 store
     return useMemo(() => {
       // 使用 immer 进行更新状态, 确保未更新的对象还是旧的引用
-      const dispatch = fn => setState(immer(state, v => fn(v)));
-
+      store.dispatch = fn => setState(immer(state, v => fn(v)));
       store.state = state;
-      store.dispatch = dispatch;
 
       return <store.Provider value={state} {...rest} />;
-      // eslint-disable-next-line
-    }, [state]);
+    }, [rest, state]);
   };
 
-  const Consumer = ({ children, memo, ...rest }) => {
+  // 创建一个消费者组件
+  const Consumer = ({ children, memo }) => {
     const state = useContext(store);
 
-    // 使用外部控制 memo
+    // 使用props控制 memo
     if (typeof memo === 'function') {
       const memoList = memo(state);
-      // eslint-disable-next-line
+
       return useMemo(() => {
         return children(state, store.dispatch);
-        // eslint-disable-next-line
-      }, memoList);
-    } else {
-      return children(state, store.dispatch);
+      }, [children, state]);
     }
+    return children(state, store.dispatch);
   };
 
   return { Provider, store, Consumer };
